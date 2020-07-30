93585d6191ac0075a5828e9282e137eb29fdd99b
Tilpo <wh.voorhaar@gmail.com>
2020-07-30 18:21:01 +0200

    mock sage

--- docs/conf.py
+++ docs/conf.py
@@ -65,6 +65,8 @@ napoleon_use_ivar = True
 napoleon_use_param = True
 napoleon_use_rtype = True
 
+autodoc_mock_imports = ["sage"]
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
